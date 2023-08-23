9088ad556c9a4ab5261367d1e6f71d06707b36e5
amir-roshan <amirvalielahi@gmail.com>
2023-08-23 09:53:41 -0700

    Add Rotation while the player is moving into x o y axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] InputAction movement;
+
     [SerializeField] float controlSpeed = 10f;
     [SerializeField] float xRange = 10f;
-    [SerializeField] float yRange = 7f;
+    [SerializeField] float yRange = 9f;
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = -2f;
+    [SerializeField] float controlRollFactor = 20f;
+
+    [SerializeField] float rotationFactor = .5f; // Adjust this based on how fast you want the rotation to occur
+
+    float xThrow, yThrow;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,21 +38,39 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         ProcessTranslation();
-
+        ProcessRotation();
     }
 
     void ProcessTranslation()
     {
-        float xThrow = movement.ReadValue<Vector2>().x;
+        xThrow = movement.ReadValue<Vector2>().x;
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXpos = transform.localPosition.x + xOffset;
         float clampedXpos = Mathf.Clamp(rawXpos, -xRange, xRange);
 
-        float yThrow = movement.ReadValue<Vector2>().y;
+        yThrow = movement.ReadValue<Vector2>().y;
         float yOffset = yThrow * Time.deltaTime * controlSpeed;
         float rawYpos = transform.localPosition.y + yOffset;
-        float clampedYpos = Mathf.Clamp(rawYpos, -yRange, yRange);
+        float clampedYpos = Mathf.Clamp(rawYpos, -yRange, yRange); // Adding boundaries to our movement
+
         transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
     }
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
 
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+
+        float rollDueToControlThrow = xThrow * controlRollFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = yawDueToPosition;
+        float roll = rollDueToControlThrow;
+
+        Quaternion targetRotation = Quaternion.Euler(pitch, yaw, roll);
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, targetRotation, rotationFactor);
+        //  returns a new rotation that gradually moves from the current rotation towards the target rotation.
+
+    }
 }
