fc221c27acfcc13c83d83391368f78ad7ce0def4
amir-roshan <amirvalielahi@gmail.com>
2023-08-22 23:40:40 -0700

    Move the player based on the input and set boundaries for x and y axis.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 7f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+    private void OnEnable() // it starts after awake and before start function 
+    {
+        movement.Enable();
+    }
+    private void OnDisable()
+    {
+        movement.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessTranslation();
+
     }
+
+    void ProcessTranslation()
+    {
+        float xThrow = movement.ReadValue<Vector2>().x;
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXpos = transform.localPosition.x + xOffset;
+        float clampedXpos = Mathf.Clamp(rawXpos, -xRange, xRange);
+
+        float yThrow = movement.ReadValue<Vector2>().y;
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float rawYpos = transform.localPosition.y + yOffset;
+        float clampedYpos = Mathf.Clamp(rawYpos, -yRange, yRange);
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
+    }
+
 }
